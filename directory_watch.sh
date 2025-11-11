a6686368c73b4e25ac78d1f4ed43fdf561377c62
Lok Yung Chan <lokyung.chan@ontariotechu.net>
2025-11-11 18:17:57 -0500

    added user input to confirm automation setup

--- directory_watch.sh
+++ directory_watch.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 
-if [ "$PPID" -eq 1 ]; then
-
-else #when the script is ran manually
-	#the directory where the user n the script manually
-	initial_directory="$pwd"
-
-	#make directory for log file if it doesn't exist
-	mkdir -p "${initial_directory}/Directory_Watcher_Log"
-
-	# log destination file:
-	file="${initial_directory}/Directory_Watcher_Log/directory_change_log.json"
-fi
-
 #first argument as the directory to monitor
 target_directory="${1:-.}"
-
 #check directory validity
 if [ ! -d "$target_directory" ]; then
 	echo "The directory you entered does not exist"
 	exit 0
 else 
 	echo "Directory Watcher is now running..."
+fi
+
+
+if [ "$PPID" -eq 1 ]; then
+	file
+else #when the script is ran manually
+	#define the directory where the user run the script manually
+	initial_directory="$pwd"
+
+	#setup a directory for log file if it doesn't exist
+	mkdir -p "${initial_directory}/Directory_Watcher_Log"
+
+	# define the log folder in the log directory:
+	file="${initial_directory}/Directory_Watcher_Log/directory_change_log.json"
+
+	#auto start configuration
+	read -p "would you like the script to run automatically on startup?(y/n)" input
+	input="${input,,}"
+
+	if [["$input" -eq "y"]]; then
+		#define script address
+		file_address = "${initial_directory}/directory_watch.sh"
+		target_directory = "${initial_directory}/${target_directory}"
+		
+		break
+	else if [["$input" -eq "n"]]; then
+		echo "skipping cron setup"
+		break
+	else
+		echo "invalid input, please enter y or n"
+	fi
 fi
 
 #===logging logic===
