52113ca6d711cddbf1ac7f6d19bd7ba8d6153376
cc555cc <lokyung.chan@ontariotechu.net>
2025-11-26 00:10:46 -0500

    updated to Syed's version

--- directory_watch.sh
+++ directory_watch.sh
@@ -5,74 +5,44 @@ target_directory="${1:-.}"
 target_directory="$(realpath "$target_directory")"
 #check directory validity
 if [ ! -d "$target_directory" ]; then
-	echo "The directory you entered does not exist"
+	echo "The directory you entered does not exist, exiting..."
 	exit 1
 fi
 
-#when script is ran by crontab
-if [ "$CRON" = "1" ]; then
-	sleep 10
-	echo "crontab run";
-	mkdir -p "$2/Directory_Watcher_Log"
-	basename="$(basename "$1")"
-	file="$2/Directory_Watcher_Log/${basename}_change_log.jsonl"
-else #when the script is ran manually
-	echo "manual run"
-	#define the directory where the user run the script manually
-	initial_directory="$(pwd)"
+filename="$2"; #name of the output file
 
-	#setup a directory for log file if it doesn't exist
-	mkdir -p "${initial_directory}/Directory_Watcher_Log"
-
-	#extract name of target_directory
-	dirname="$(basename "$target_directory")"
-
-	# define the log folder in the log directory:
-	file="${initial_directory}/Directory_Watcher_Log/${dirname}_change_log.jsonl"
-
-	#auto start configuration
-	read -p "would you like the script to run automatically on startup?(y/n)" input
-	input="${input,,}"
-
-	if [[ "$input" == "y" ]]; then
-		echo "you accepted auto start"
-		echo "setting up in crontab now..."
-		#define script address
-		script_address="${initial_directory}/directory_watch.sh"
-		cron_entry="@reboot CRON=1 ${script_address} ${target_directory} ${initial_directory} ${initial_directory}/Directory_Watcher_Log"
-
-		#check if a watcher for this directory already registered in crontab on this device
-		crontab -l 2>/dev/null | grep -F "$target_directory" >/dev/null
-
-		if [ $? -ne 0 ]; then #no duplicate entry
-			(crontab -l 2>/dev/null; echo "$cron_entry") | crontab -
-			echo "wrote $cron_entry successfully";
-		else
-			echo "cron entry for $watch_directory already exists"
-		fi
-
-	elif [[ "$input" == "n" ]]; then
-		echo "skipping cron setup";
-	else
-		echo "invalid input, please enter y or n";
-	fi
+#if 2nd argument is not valid, set a default value
+if [[ ! "$2" =~ ^[a-zA-Z0-9._-]+$ ]]; then
+  echo "Warning: Invalid or missing file name for log output, using default name...";
+  filename="$(basename "$(realpath "$target_directory")")_change_log.json";
 fi
+
+#define the directory where the user run the script manually
+initial_directory="$(pwd)"
+
+#setup a directory for log file if it doesn't exist
+mkdir -p "${initial_directory}/Directory_Watcher_Log"
+
+# define the log folder in the log directory:
+file="${initial_directory}/Directory_Watcher_Log/$filename"
+
+echo "";
 
 #===logging logic===
 /usr/bin/inotifywait -m -r -e create -e modify -e delete -e move \
 	--format '%T|%w%f|%e' --timefmt '%F %T' "$target_directory" | while IFS="|" read DATE FILE EVENT
-
-do
+do	
 	#entry sanitization for JSON format: turns " to \" and \ to \\
 	sanitized_file_name=$(printf '%s' "$FILE" | sed 's/\\/\\\\/g; s/"/\\"/g')
-	sanitized_event=$(printf '%s' "$EVENT" | sed 's/\\/\\\\/g; s/"/\\"/g')
-
+	
+	#echo the event
+	echo "$DATE File: $sanitized_file_name $EVENT";	
 	entry=$(cat <<EOF
 {
 	"timestamp": "$DATE",
 	"file": "$sanitized_file_name",
-	"event": "$sanitized_event"
-}
+	"event": "$EVENT"
+},
 EOF
 )
 	
